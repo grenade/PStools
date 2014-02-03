330905d53f23062744452ca33a581bb54117e26b
gregzakharov <grishanz@yandex.ru>
2014-02-03 19:44:43 +0600

    Update Get-SystemInstallDate.ps1

--- Management/Get-SystemInstallDate.ps1
+++ Management/Get-SystemInstallDate.ps1
@@ -1,14 +1,18 @@
 #requires -version 2.0
-function Get-SystemInstallDate {
+function Get-SystemInstallDate([String]$Computer = '.') {
   <#
     .NOTES
         Author: greg zakharov
   #>
   
   try {
-    ([wmi]'').ConvertToDateTime((gwmi Win32_OperatingSystem).InstallDate)
+    [Management.ManagementDateTimeConverter]::ToDateTime(
+      ((New-Object Management.ManagementClass(
+        [Management.ManagementPath]('\\' + $Computer + '\root\cimv2:Win32_OperatingSystem')
+      )).PSBase.GetInstances() | select InstallDate).InstallDate
+    )
   }
-  catch [Management.Automation.RuntimeException] {
+  catch [Management.Automation.MethodInvocationException] {
     if ($_.Exception) {
       [TimeZone]::CurrentTimeZone.ToLocalTime([DateTime]'1.1.1970').AddSeconds(
         (gp 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').InstallDate
